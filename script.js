298cc1a2bd38070f6931b9bb82ef9945e73d18fa
heloyse1406 <heloyse.camile@escola.pr.gov.br>
2025-04-29 11:50:16 -0300

    29/04

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ document.addEventListener('DOMContentLoaded', function() {
         botaoDeAcessibilidade.classList.toggle(rotacao-botao)
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
-        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
-        botaoDeAcessibilidade.setAttribute('aria-expanded'), !botaoSelecionado
+        const botaoSelecionado = botaoDeAcessibilidade. getAttribute ('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
     })
 
-    const aumenteForteBotao = document.getElementById('aumentar-fonte')
+     const aumenteForteBotao = document.getElementById('aumentar-fonte');
+     const diminuiForteBotao = document.getElementById('diminuir-forte');
+
+    let tamanhoAtualForte = 1;
+     aumenteForteBotao.addEventListener('click', function(){
+        tamanhoAtualForte += 0.1;
+        document.body.style.fontSize = '${tamanhoAtualForte}rem'
+     })
+     diminuiForteBotao.addEventListener('click', function(){
+        tamanhoAtualForte -= 0.1;
+        document.body.style.fontSize = '$(tamanhoAtualForte}rem'
+     })
+    
 })
